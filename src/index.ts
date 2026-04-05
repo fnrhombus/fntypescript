5eaf3b119e6f97b2c32440fa48be4d96c00d9917
fnrhombus <2511516+fnrhombus@users.noreply.github.com>
2026-04-05 02:34:03 -0400

    feat: implement plugin init/create entry point (#9)
    Implements the tsserver plugin entry point with init/create/getExternalFiles,
    delegating language service calls through createLanguageServiceProxy.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export {};
+import type ts from "typescript/lib/tsserverlibrary";
+import { createLanguageServiceProxy } from "./proxy.js";
+
+function init(modules: {
+  typescript: typeof import("typescript/lib/tsserverlibrary");
+}): ts.server.PluginModule {
+  void modules;
+
+  function create(info: ts.server.PluginCreateInfo): ts.LanguageService {
+    void info.config;
+    return createLanguageServiceProxy(info.languageService);
+  }
+
+  function getExternalFiles(_project: ts.server.Project): string[] {
+    return [];
+  }
+
+  return { create, getExternalFiles };
+}
+
+export = init;
